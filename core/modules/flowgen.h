185a1ab437d5a11c7ed5bfe2226328f7a1170fda
Justine Sherry <sherry@nefeli.io>
2017-01-26 13:53:25 -0800

    checking in to switch branch lol

--- core/modules/flowgen.h
+++ core/modules/flowgen.h
@@ -10,7 +10,11 @@
 
 typedef std::pair<uint64_t, struct flow *> Event;
 typedef std::priority_queue<Event, std::vector<Event>,
-                            std::function<bool(Event, Event)>> EventQueue;
+        std::function<bool(Event, Event)>> EventQueue;
+
+const Commands FlowGen::cmds = {
+  {"update", "FlowGenArg", MODULE_CMD_FUNC(&FlowGen::CommandUpdate), 0}
+};
 
 struct flow {
   uint32_t flow_id;
@@ -20,98 +24,99 @@ struct flow {
 };
 
 class FlowGen final : public Module {
- public:
-  enum Arrival {
-    ARRIVAL_UNIFORM = 0,
-    ARRIVAL_EXPONENTIAL,
-  };
+  public:
+    enum Arrival {
+      ARRIVAL_UNIFORM = 0,
+      ARRIVAL_EXPONENTIAL,
+    };
 
-  enum Duration {
-    DURATION_UNIFORM = 0,
-    DURATION_PARETO,
-  };
+    enum Duration {
+      DURATION_UNIFORM = 0,
+      DURATION_PARETO,
+    };
 
-  static const gate_idx_t kNumIGates = 0;
+    static const gate_idx_t kNumIGates = 0;
 
-  FlowGen()
+    FlowGen()
       : Module(),
-        active_flows_(),
-        allocated_flows_(),
-        generated_flows_(),
-        flows_(),
-        flows_free_(),
-        events_(),
-        templ_(),
-        template_size_(),
-        rng_(),
-        arrival_(),
-        duration_(),
-        quick_rampup_(),
-        total_pps_(),
-        flow_rate_(),
-        flow_duration_(),
-        concurrent_flows_(),
-        flow_pps_(),
-        flow_pkts_(),
-        flow_gap_ns_(),
-        pareto_() {}
+      active_flows_(),
+      allocated_flows_(),
+      generated_flows_(),
+      flows_(),
+      flows_free_(),
+      events_(),
+      templ_(),
+      template_size_(),
+      rng_(),
+      arrival_(),
+      duration_(),
+      quick_rampup_(),
+      total_pps_(),
+      flow_rate_(),
+      flow_duration_(),
+      concurrent_flows_(),
+      flow_pps_(),
+      flow_pkts_(),
+      flow_gap_ns_(),
+      pareto_() {}
 
-  pb_error_t Init(const bess::pb::FlowGenArg &arg);
+    pb_error_t Init(const bess::pb::FlowGenArg &arg);
+    pb_error_t CommandUpdate(const bess:pb::FlowGenArg &arg);
 
-  void DeInit() override;
+    void DeInit() override;
 
-  struct task_result RunTask(void *arg) override;
+    struct task_result RunTask(void *arg) override;
 
-  std::string GetDesc() const override;
+    std::string GetDesc() const override;
 
- private:
-  inline double NewFlowPkts();
-  inline double MaxFlowPkts() const;
-  inline uint64_t NextFlowArrival();
-  inline struct flow *ScheduleFlow(uint64_t time_ns);
-  void MeasureParetoMean();
-  void PopulateInitialFlows();
-  bess::Packet *FillPacket(struct flow *f);
-  void GeneratePackets(bess::PacketBatch *batch);
+  private:
+    inline double NewFlowPkts();
+    inline double MaxFlowPkts() const;
+    inline uint64_t NextFlowArrival();
+    inline struct flow *ScheduleFlow(uint64_t time_ns);
+    void MeasureParetoMean();
+    void PopulateInitialFlows();
+    bess::Packet *FillPacket(struct flow *f);
+    void GeneratePackets(bess::PacketBatch *batch);
 
-  pb_error_t InitFlowPool();
-  pb_error_t ProcessArguments(const bess::pb::FlowGenArg &arg);
+    pb_error_t InitFlowPool();
+    pb_error_t ProcessArguments(const bess::pb::FlowGenArg &arg);
 
-  int active_flows_;
-  int allocated_flows_;
-  uint64_t generated_flows_;
-  struct flow *flows_;
-  std::deque<struct flow *> flows_free_;
+    int active_flows_;
+    int allocated_flows_;
+    uint64_t generated_flows_;
+    struct flow *flows_;
+    std::deque<struct flow *> flows_free_;
 
-  EventQueue events_;
+    EventQueue events_;
 
-  char *templ_;
-  int template_size_;
+    char *templ_;
+    int template_size_;
 
-  Random rng_;
+    Random rng_;
 
-  Arrival arrival_;
-  Duration duration_;
+    Arrival arrival_;
+    Duration duration_;
 
-  /* behavior parameters */
-  int quick_rampup_;
+    /* behavior parameters */
+    int quick_rampup_;
 
-  /* load parameters */
-  double total_pps_;
-  double flow_rate_;     /* in flows/s */
-  double flow_duration_; /* in seconds */
+    /* load parameters */
+    double total_pps_;
+    double flow_rate_;     /* in flows/s */
+    double flow_duration_; /* in seconds */
 
-  /* derived variables */
-  double concurrent_flows_; /* expected # of flows */
-  double flow_pps_;         /* packets/s/flow */
-  double flow_pkts_;        /* flow_pps * flow_duration */
-  double flow_gap_ns_;      /* == 10^9 / flow_rate */
+    /* derived variables */
+    double concurrent_flows_; /* expected # of flows */
+    double flow_pps_;         /* packets/s/flow */
+    double flow_pkts_;        /* flow_pps * flow_duration */
+    double flow_gap_ns_;      /* == 10^9 / flow_rate */
 
-  struct {
-    double alpha;
-    double inversed_alpha; /* 1.0 / alpha */
-    double mean;           /* determined by alpha */
-  } pareto_;
+    struct {
+      double alpha;
+      double inversed_alpha; /* 1.0 / alpha */
+      double mean;           /* determined by alpha */
+    } pareto_;
 };
 
 #endif  // BESS_MODULES_FLOWGEN_H_
